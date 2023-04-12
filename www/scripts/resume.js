7f109122e946cfbff74c38777eea7e20e43c7dc2
Alison Gale <agale753@gmail.com>
2023-04-12 09:05:07 -0700

    Add more polish to the resume

--- www/scripts/resume.js
+++ www/scripts/resume.js
@@ -3,3 +3,7 @@ links.innerHTML =
   anchor("https://www.linkedin.com/in/alison-gale/", LINKEDIN) +
   anchor("https://github.com/agale123", GITHUB) +
   anchor("https://twitter.com/agale137", TWITTER);
+
+  document.querySelectorAll(".link").forEach(link => {
+    link.innerHTML = LINK;
+  });
